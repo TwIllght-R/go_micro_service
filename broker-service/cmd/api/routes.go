1dcdc16e413220090f14534d66793cc517ff7db3
TwIllght-R <nathawutrock@gmail.com>
2024-05-04 16:05:10 +0700

    updated gRPC for broker service

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -23,6 +23,7 @@ func (app *Config) routes() http.Handler {
 	mux.Use(middleware.Heartbeat("/ping"))
 	mux.Post("/", app.Broker)
 	mux.Post("/handle", app.HandleSubmisstion)
+	mux.Post("/log-grpc", app.LogViaGRPC)
 
 	return mux
 
